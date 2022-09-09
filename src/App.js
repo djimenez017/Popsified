12980f0c62584fa04d9ef37833599b452a615326
Daniel Jimenez <djimenez017@gmail.com>
2022-09-09 01:14:34 -0700

    implement routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import Header from './components/Header';
-import Footer from './components/Footer'
-import './App.css';
-import {Container} from "react-bootstrap"
- 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <main>
         <Container>
           <h1>Welcome to Posified</h1>
-        </Container> 
+          <HomeScreen />
+        </Container>
+      </main>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;

8c492126f11782376a26bdbf4bf7b87100c4517a
Daniel Jimenez <daniel@Daniels-MacBook-Pro.local>
2022-09-30 00:42:34 -0700

    working on redux for state management

--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
+import { productListReducers } from "./reducers/productReducers";
+
+const reducer =  combineReducers(
+  {
+    productList: productListReducers
+  }
+)
 
 export default configureStore({
-  reducer: {},
+  reducer,
   preloadedState: {},
   middleware: [thunk],
   //devTools: process.env.NODE_ENV !== "production",

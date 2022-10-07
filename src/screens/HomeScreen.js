103d2abc1fe01b726c3ff60834a7cac3e073a422
Daniel Jimenez <daniel@Daniels-MacBook-Pro.local>
2022-10-06 20:21:51 -0700

    fixed issues when retrieving data

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,24 +13,33 @@ const HomeScreen = () => {
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
-  console.log(products);
+
+  // console.log(products);
+
   return (
-    <>
-      <h1>Latest Products</h1>
-      {loading ? (
-        <h2>Loading...</h2>
-      ) : error ? (
-        <h3>{error}</h3>
-      ) : (
-        <Row>
-          {products.data.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
-      )}
-    </>
+    <Row>
+    {products.map((product) => (
+      <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+        <Product product={product} />
+      </Col>
+    ))}
+  </Row>
+    // <>
+    //   <h1>Latest Products</h1>
+    //   {loading ? (
+    //     <h2>Loading...</h2>
+    //   ) : error ? (
+    //     <h3>{error}</h3>
+    //   ) : (
+    //     <Row>
+    //       {products.data.map((product) => (
+    //         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+    //           <Product product={product} />
+    //         </Col>
+    //       ))}
+    //     </Row>
+    //   )}
+    // </>
   );
 };
 

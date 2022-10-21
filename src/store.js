72a65c6c01d0b33964874466d196ce1707cb73cb
Daniel Jimenez <djimenez017@gmail.com>
2022-10-20 20:22:22 -0700

    work on cart functionality

--- src/store.js
+++ src/store.js
@@ -4,15 +4,21 @@ import {
   productListReducers,
   productDetailReducers,
 } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
   productList: productListReducers,
   productDetails: productDetailReducers,
+  cart: cartReducer,
 });
+
+const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
 
 export default configureStore({
   reducer,
-  preloadedState: {},
+  preloadedState: { cart: { cartItems: cartItemsFromLocalStorage } },
   middleware: [thunk],
   //devTools: process.env.NODE_ENV !== "production",
 });
